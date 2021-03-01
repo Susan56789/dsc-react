00140143496c079f471502ad91bcd8c5bf845c8b
sueNimoh <mwihaki2susan@gmail.com>
2021-03-01 09:04:54 +0300

    My first react App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Apples from "./Apples";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const name ="Susan";
+    const day ="Friday";
+
+    return (
+        <div>
+            <h1>Shopping List for {name} to be done on {day}</h1>
+            <ul>
+                <li>Apple</li>
+                <li>Banana</li>
+                <li>Cake</li>
+                <li>Juice</li>
+                <li>Salt</li>
+            </ul>
+            <Apples/>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/Apples.js
@@ -0,0 +1,10 @@
+function Apples(){
+    return(
+        <div>
+            <p>Apples</p>
+            <p>More apples</p>
+            <p>Even more apples</p>
+        </div>
+    );
+}
+export default Apples;
